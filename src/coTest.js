aeb3c8b0bc6fb6e680e6382ac9f8b4c503ed857b
Andrew4d3 <andrew4d3@gmail.com>
2018-02-28 17:49:58 -0300

    Implementing handlers

--- src/coTest.js
+++ src/coTest.js
@@ -9,49 +9,80 @@ class Product {
 class CarInsurance {
   constructor(products = []) {
     this.products = products;
+    this.normalDecrementer = 1;
   }
 
   getProductHandler(i) {
     const { name } = this.products[i];
     if (name === 'Full Coverage') {
-      return this.updateFullCoverage;
+      return this.updateFullCoverage.bind(this, i);
     } else if (name === 'Mega Coverage') {
-      return this.updateMegaCoverage;
+      return this.updateMegaCoverage.bind(this, i);
     } else if (name === 'Special Full Coverage') {
-      return this.updateSpecialFullCoverage;
+      return this.updateSpecialFullCoverage.bind(this, i);
     } else if (name === 'Super Sale') {
-      return this.updateSuperSale;
+      return this.updateSuperSale.bind(this, i);
     } else {
-      return this.updateNormal;
+      return this.updateNormal.bind(this, i);
     }
   }
 
   updateNormal(i) {
-    return this.products[i]
+    const product = this.products[i];
+    const decrementer = product.sellIn > 0 ? this.normalDecrementer : this.normalDecrementer * 2;
+    product.price = product.price > 0 ? product.price - decrementer : product.price;
+    product.price = product.price < 0 ? 0 : product.price;
+    product.sellIn -= 1;
   }
 
   updateFullCoverage(i) {
-    return this.products[i]
+    const product = this.products[i];
+    const incrementer = product.sellIn > 0 ? 1 : 2;
+    product.price = product.price < 50 ? product.price + incrementer : product.price;
+    product.price = product.price > 50 ? 50 : product.price;
+    product.sellIn -= 1;
   }
 
   updateMegaCoverage(i) {
-    return this.products[i]
+    // DO NOTHING
   }
 
   updateSpecialFullCoverage(i) {
-    return this.products[i]
+    const product = this.products[i];
+    if (product.sellIn > 10) {
+      product.price = product.price < 50 ? product.price + 1 : product.price;
+    } else if (product.sellIn <= 10 && product.sellIn > 5) {
+      product.price = product.price < 50 ? product.price + 2 : product.price;
+    } else if (product.sellIn <= 5 && product.sellIn > 0) {
+      product.price = product.price < 50 ? product.price + 3 : product.price;
+    } else {
+      product.price = 0;
+    }
+    product.price = product.price > 50 ? 50 : product.price;
+    product.sellIn -= 1;
   }
 
   updateSuperSale(i) {
-    return this.products[i]
+    const product = this.products[i];
+    const decrementer = product.sellIn > 0 ? this.normalDecrementer * 2 : this.normalDecrementer * 4;
+    product.price = product.price > 0 ? product.price - decrementer : product.price;
+    product.price = product.price < 0 ? 0 : product.price;
+    product.sellIn -= 1;
   }
 
   updatePrice() {
-    return this.products
+    const { products } = this;
+
+    products.forEach((product, i) => {
+      const handler = this.getProductHandler(i);
+      handler();
+    });
+
+    return this.products;
   }
 }
 
 module.exports = {
   Product,
   CarInsurance
-}
+};
